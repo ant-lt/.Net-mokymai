ebd59d36d4d37f4f76f376ae80a523f697facef7
Vitas Gruzdas <vitas.gruzdas@codeacademylt.onmicrosoft.com>
2022-06-06 20:17:53 +0300

    konkatinacija, kompozicija,  interpoliacija

--- BasicMokymai/BasicMokymai/Program.cs
+++ BasicMokymai/BasicMokymai/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.Write("Išvedimas ");
+            Console.Write("vienoje ");
+            Console.Write("eilutėje ");
+            Console.WriteLine();
+            Console.WriteLine("tekstas kitoje eilutėje ");
+            Console.Write("Tekstas");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Išvedimas " + "vienoje " + "eilutėje"); // konkatinacija
+            Console.WriteLine("{0} {1} {2}", "Išvedimas", "vienoje", "eilutėje"); // kompozicija
+            Console.WriteLine($"{"Išvedimas"} {"vienoje"} { "eilutėje"}"); // interpoliacija
+            Console.WriteLine("------------------------------");
+
+
+            Console.ReadKey();
         }
     }
 }
