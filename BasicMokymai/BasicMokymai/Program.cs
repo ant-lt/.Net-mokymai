554e6cd285d8eb634497da027d3e47f61d486e96
Vitas Gruzdas <vitas.gruzdas@codeacademylt.onmicrosoft.com>
2022-06-06 21:27:51 +0300

    informacijos įvedimas

--- BasicMokymai/BasicMokymai/Program.cs
+++ BasicMokymai/BasicMokymai/Program.cs
@@ -18,8 +18,37 @@
             Console.WriteLine($"{"Išvedimas"} {"vienoje"} { "eilutėje"}"); // interpoliacija
             Console.WriteLine("------------------------------");
 
+            Console.WriteLine("tekstas idedamas i \"kabutes\"");
+            Console.WriteLine($"tekstas {Environment.NewLine}idedamas \r\ni \r\n\"kabutes\"");
+            Console.WriteLine("tekstas \tidedamas \ti \t\"kabutes\"");
+
+            Console.WriteLine("The \u00c6olean & Hard\", by Samuel Taylor Coleridge ");
+
+            Console.WriteLine("-----Press any key to continue----");
 
             Console.ReadKey();
+            Console.Clear();
+
+            //Console.WriteLine("Įveskite savo vardą, o aš jį pakartosiu:");
+            // Console.WriteLine("o štai mano pakartojimas " + Console.ReadLine());
+            //Console.WriteLine("o štai mano pakartojimas {0}",  Console.ReadLine());
+            //Console.WriteLine($"o štai mano pakartojimas  {Console.ReadLine()}");
+
+            //Console.WriteLine("Įveskite raidę");
+            //var key = Console.ReadKey();
+
+            //Console.WriteLine("Ivestas simbolis {0}", key.KeyChar);
+            //Console.WriteLine("Ivestas simbolis {0}", key.Key );
+            //Console.WriteLine("Ivestas simbolis {0}", (int)key.KeyChar);
+
+            //Console.WriteLine("Ivestas simbolis {0}", Console.ReadKey().KeyChar);
+            //Console.WriteLine("Ivestas simbolis {0}", (int)Console.ReadKey().KeyChar);
+
+            Console.WriteLine("Įveskite savo vardą, o aš atspėsiu pirmą raidę:");
+            Console.WriteLine("o štai mano spejimas\"" + Console.ReadLine()[0] + "\"");
+
+            Console.ReadKey();
+
         }
     }
 }
