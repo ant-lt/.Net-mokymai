63cc704c85a73f97688e6ee9ace7fd0a23b364c1
Vitas Gruzdas <vitas.gruzdas@codeacademylt.onmicrosoft.com>
2022-06-07 17:29:43 +0300

    Užduotis nr. 1 pabaigta

--- BasicMokymai/BasicMokymai/Program.cs
+++ BasicMokymai/BasicMokymai/Program.cs
@@ -43,11 +43,55 @@
 
             //Console.WriteLine("Ivestas simbolis {0}", Console.ReadKey().KeyChar);
             //Console.WriteLine("Ivestas simbolis {0}", (int)Console.ReadKey().KeyChar);
-
+            /*
             Console.WriteLine("Įveskite savo vardą, o aš atspėsiu pirmą raidę:");
             Console.WriteLine("o štai mano spejimas\"" + Console.ReadLine()[0] + "\"");
 
             Console.ReadKey();
+
+            Console.WriteLine("eilute 1" + 
+                 Environment.NewLine + "eilute 2" +
+                 Environment.NewLine + "eilute 3");
+
+            Console.WriteLine(@"eilute 1
+              eilute 2
+              eilute 3");
+       */
+
+            Console.WriteLine("--------");
+            Console.WriteLine("Standard Numeric Format Specifiers");
+            Console.WriteLine(
+           "(C) Currency: . . . . . . . . {0:C}\n" +
+           "(D) Decimal:. . . . . . . . . {0:D}\n" +
+           "(E) Scientific: . . . . . . . {1:E}\n" +
+           "(F) Fixed point:. . . . . . . {1:F}\n" +
+           "(G) General:. . . . . . . . . {0:G}\n" +
+           "    (default):. . . . . . . . {0} (default = 'G')\n" +
+           "(N) Number: . . . . . . . . . {0:N}\n" +
+           "(P) Percent:. . . . . . . . . {1:P}\n" +
+           "(R) Round-trip: . . . . . . . {1:R}\n" +
+           "(X) Hexadecimal:. . . . . . . {0:X}\n",
+           -123, -123.45f);
+
+            Console.WriteLine("--------");
+            Console.WriteLine("Standard DateTime Format Specifiers");
+            Console.WriteLine(
+                "(d) Short date: . . . . . . . {0:d}\n" +
+                "(D) Long date:. . . . . . . . {0:D}\n" +
+                "(t) Short time: . . . . . . . {0:t}\n" +
+                "(T) Long time:. . . . . . . . {0:T}\n" +
+                "(f) Full date/short time: . . {0:f}\n" +
+                "(F) Full date/long time:. . . {0:F}\n" +
+                "(g) General date/short time:. {0:g}\n" +
+                "(G) General date/long time: . {0:G}\n" +
+                "    (default):. . . . . . . . {0} (default = 'G')\n" +
+                "(M) Month:. . . . . . . . . . {0:M}\n" +
+                "(R) RFC1123:. . . . . . . . . {0:R}\n" +
+                "(s) Sortable: . . . . . . . . {0:s}\n" +
+                "(u) Universal sortable: . . . {0:u} (invariant)\n" +
+                "(U) Universal full date/time: {0:U}\n" +
+                "(Y) Year: . . . . . . . . . . {0:Y}\n",
+                DateTime.Now);
 
         }
     }
---
+++ BasicMokymai/Užduotis nr.1/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Užduotis_nr._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.1 −Išveskite į konsolę savo vardą
+            Console.Clear();
+            Console.WriteLine("Užduotis 1.1 −Išveskite į konsolę savo vardą");
+            Console.WriteLine("Vitas!");
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            //1.2 −Nuskaitykite iš klaviatūros savo vardą ir išveskite į konsolę
+            //Console.Clear();
+            Console.WriteLine("Užduotis 1.2 −Nuskaitykite iš klaviatūros savo vardą ir išveskite į konsolę");
+            Console.WriteLine("Iveskite savo vardą:");
+            Console.WriteLine("Jusu vardas:{0}", Console.ReadLine());
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            //1.3 −Nuskaitykite iš klaviatūros savo vardo pirmąją raidę ir išveskite į konsolę jos ASCII kodą
+            //Console.Clear();
+
+            Console.WriteLine("Užduotis 1.3 −Nuskaitykite iš klaviatūros savo vardo pirmąją raidę ir išveskite į konsolę jos ASCII kodą");
+            Console.WriteLine("Iveskite savo vardo pirmąją raidę:");
+            Console.WriteLine("Pirmos raides ASCII kodas \"{0}\"", (int)Console.ReadKey().KeyChar);
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            //1.4 - Nuskaitykite iš klaviatūros savo vardo pirmąją raidę, tada nuskaitykite bet kokį skaičių ir išveskite į konsolę sumą.
+            //Pabandykite šį skaičių išvesti įvairiais formatais
+            // Console.Clear();
+          
+            Console.WriteLine("Užduotis 1.4 - Nuskaitykite iš klaviatūros savo vardo pirmąją raidę, tada nuskaitykite bet kokį skaičių ir išveskite į konsolę sumą");
+            Console.WriteLine("Iveskite savo vardo pirmąją raidę:");
+            var vardo_raides_kodas = (int)Console.ReadKey().KeyChar;
+
+            Console.WriteLine("Iveskite bet kokį skaičių:");
+
+            var skaicius = Console.ReadLine();
+
+            int skaicius2;
+            skaicius2 = Convert.ToInt32(skaicius);
+
+            var suma = vardo_raides_kodas + skaicius2;
+            Console.WriteLine(
+            "Pirmos raides ASCII kodas: ...........{0:G}\n" +
+            "Skaičius:. . . . . . . . .............{1} (default = 'G')\n" +
+            "Suma {0} + {1} =  {3} be konvertavimo \n" +
+            "Suma {0} + {1} =  {2:C} formatas (C) Currency\n" +
+            "Suma {0} + {1} =  {2:D} formatas (D) Decimal\n" +
+            "Suma {0} + {1} =  {2:G} formatas (G) General\n" +
+            "Suma {0} + {1} =  {2:N} formatas (N) Number\n",
+            vardo_raides_kodas, skaicius, suma, vardo_raides_kodas + skaicius);
+            Console.WriteLine("---------------------------------------------------------------------------");
+
+            //1.5 - Padarykite konsolės meniu skirtingose eilutėse (1) Pirkti, (2) Parduoti, (3) Likučiai. Išveskite pasirinktą meniu punktą.
+            // Console.Clear();
+            Console.WriteLine("Užduotis 1.5 - Padarykite konsolės meniu skirtingose eilutėse (1) Pirkti, (2) Parduoti, (3) Likučiai. Išveskite pasirinktą meniu punktą.");
+
+            Console.WriteLine(
+            "Pasirinkite meniu:" + Environment.NewLine +
+            "(1) Pirkti" + Environment.NewLine +
+            "(2) Parduoti" + Environment.NewLine +
+            "(3) Likučiai" + Environment.NewLine);
+
+            var pasirinktas_meniu = Console.ReadKey().KeyChar;
+            Console.WriteLine("Pasirinktas: {0}", pasirinktas_meniu) ;
+
+        }
+    }
+}
