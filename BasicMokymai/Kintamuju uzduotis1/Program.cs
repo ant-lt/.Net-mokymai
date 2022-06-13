5c2a25adf83d474ca6e0f2508a9c738bf342f325
Vitas Gruzdas <vitas.gruzdas@codeacademylt.onmicrosoft.com>
2022-06-13 22:10:35 +0300

    uzduotis su kintamasiais

---
+++ BasicMokymai/Kintamuju uzduotis1/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Kintamuju_uzduotis1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * 
+             * PARAŠYTI PROGRAMĄ, KURIOJE VARTOTOJO PRAŠOMA ĮVESTI 2 SKAIČIUS.PROGRAMA TURI IŠVESTI
+            • SKAIČIŲ SUMĄ
+            • SKAIČIŲ SKIRTUMĄ
+            • SANDAUGĄ
+            • DALYBĄ
+            */
+            Console.WriteLine("Pirmas skaicius");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Antras skaicius");
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine($"suma = {skaicius1} + {skaicius2} = {skaicius1 + skaicius2} ");
+            Console.WriteLine($"skirtumas = {skaicius1} + {skaicius2} = {skaicius1 - skaicius2} ");
+            Console.WriteLine($"daugyba  = {skaicius1} + {skaicius2} = {skaicius1 * skaicius2} ");
+            Console.WriteLine($"dalyba  = {skaicius1} / {skaicius2} = {(double)skaicius1 / skaicius2} ");
+
+            /* 
+             * PARAŠYTI PROGRAMĄ, 3 SKAIČIUS. PROGRAMA TURI IŠVESTI ŠIŲ SKAIČIŲ VIDURKĮ
+             * */
+            Console.WriteLine("Pirmas skaicius");
+            skaicius1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Antras skaicius");
+            skaicius2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Trecias skaicius");
+            int skaicius3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"vidurkis = {skaicius1}, {skaicius2}, {skaicius3}  = {((double)(skaicius1 + skaicius2 + skaicius3) /3)} ");
+
+
+            /*
+            sukurkite naują kintamajį long ir prskirkite didžiausią reikšmę.
+            sukurkite naują kintamajį short ir prskirkite didžiausią reikšmę
+            - padalinkite didesnį skaičių iš mažesnio
+            - iš rezultato atimkite maksimalų long skaičių
+            - ir pridėkite maksimalų int skaičių
+               */
+
+            long kintLong = long.MaxValue;
+            short kintShort = short.MaxValue;
+
+            Console.WriteLine("Rezultatas 1 {0}",  kintLong / kintShort);
+            Console.WriteLine("Rezultatas 2 {0}", (kintLong / kintShort)-long.MaxValue );
+            Console.WriteLine("Rezultatas 3 {0}", ((kintLong / kintShort) - long.MaxValue) + int.MaxValue);
+
+
+        }
+
+    }
+}
---
+++ BasicMokymai/Matematikos operatoriai/Program.cs
@@ -0,0 +1,111 @@
+﻿
+using System.Text;
+
+Console.WriteLine("Hello, Priskyrimo operatoriai = += -= *= /=");
+int skaicius;
+int kitasSkaicius = 20;
+int nelyginisSkaicius = 5;
+
+skaicius = 10;
+kitasSkaicius = skaicius;
+
+Console.WriteLine($"skaicius = {skaicius}, kitasskaicius = {kitasSkaicius}");
+kitasSkaicius += nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+kitasSkaicius -= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+kitasSkaicius *= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+kitasSkaicius /= nelyginisSkaicius;
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+kitasSkaicius = 21;
+
+kitasSkaicius /= nelyginisSkaicius;
+
+Console.WriteLine($"kitasSkaicius = {kitasSkaicius}");
+
+
+double doubleSkaicius = 21;
+
+doubleSkaicius /= (double)nelyginisSkaicius;
+
+Console.WriteLine($"kitasSkaicius = {doubleSkaicius}");
+
+Console.WriteLine();
+Console.WriteLine("Hello, matematinis operatoriai + - * / % ++ --");
+
+// sudetis
+int suma = skaicius + kitasSkaicius;
+Console.WriteLine(" suma =  skaicius + kitasSkaicius=  {0}", suma);
+
+int atimtis = skaicius - kitasSkaicius;
+
+Console.WriteLine(" atimtis =  skaicius - kitasSkaicius=  {0}", atimtis);
+
+int daugyba = skaicius * kitasSkaicius;
+
+Console.WriteLine(" daugyba = skaicius * kitasSkaicius = {0}", daugyba);
+
+double dalyba = (double) skaicius / kitasSkaicius;
+
+Console.WriteLine(" dalyba = skaicius * kitasSkaicius = {0}", dalyba);
+
+int matematinisRezultatas = 1 + 2 - 3 + 4 + nelyginisSkaicius - skaicius;
+
+int dalybaSuLiekana = nelyginisSkaicius % 2 ;
+Console.WriteLine("dalybaSuLiekana = nelyginisSkaicius % 2 = {0}", dalybaSuLiekana);
+
+skaicius++;
+Console.WriteLine($"skaicius = {skaicius}");
+
+skaicius--;
+Console.WriteLine($"skaicius = {skaicius}");
+
+// 
+double side1 = 5.5;
+double side2 = 3.25;
+double height = 4.6;
+double area = (side1 + side2) / 2 * height;
+double areaKazkasKito = ((side1 * 2 ) + side2) / (2 * height);
+
+Console.OutputEncoding = Encoding.UTF8;
+int nulis = 0;
+int int10 = 10;
+long loang10 = 10;
+double double10 = 10;
+
+// Console.WriteLine($"int10/nulis = {int10/nulis}"); // luzta
+// Console.WriteLine($"long10/nulis = {long10/nulis}"); // luzta
+Console.WriteLine($"double10/nulis = {double10 / nulis}"); // grazina ∞
+double a = double.PositiveInfinity;
+
+Console.WriteLine($"a={a}");
+
+Console.WriteLine($"a={a==double.PositiveInfinity}");
+Console.WriteLine($"a={a == double.NegativeInfinity}");
+Console.WriteLine($"a- 500 = {a - 500}");
+
+double a1 = double.NaN;
+Console.WriteLine($"∞ / ∞ = {a/ double.PositiveInfinity}");
+
+// *** Overflow and Underflow
+
+short s1 = 30_000;
+short s2 = 30_000;
+short s3 = (short) (s1 + s2);
+
+Console.WriteLine($"s3 = {s3}");
+
+checked
+{
+     s3 = (short) (s1 + s2);
+}
+
+
+
+
+
