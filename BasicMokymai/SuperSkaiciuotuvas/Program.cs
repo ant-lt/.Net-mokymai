5274fa05ddaf90880ba22c70f18f4dddc288af04
Vitas Gruzdas <v.gruzdas74@gmail.com>
2022-07-24 17:45:29 +0300

    Super skaičiuotuvas

--- BasicMokymai/SuperSkaiciuotuvas/Program.cs
+++ BasicMokymai/SuperSkaiciuotuvas/Program.cs
@@ -59,17 +59,151 @@
         */
     public class Program
     {
+        // Globalūs kintamieji
         static double? rezultatas = null;
+        static double? skaicius1 = null;
+        static double? skaicius2 = null;
+        static int? veiksmas = null;
+        static int meniu = 0;
+        static int klaida = 0;
+        static bool testi = true;
 
         static void Main(string[] args)
         {
-
+            PagrindinisMeniu();
         }
 
 
         public static void SuperSkaiciuotuvas(string ivedimas)
         {
             //todo
+            klaida = 0;
+            if (ivedimas.Length >0 && meniu >= 0 && meniu <= 2 && (skaicius1 == null || skaicius2 == null) && veiksmas == null)
+            {
+                int ivestis= IntSkaiciausTikrinimas(ivedimas);
+                if ((meniu == 0 && ivestis >= 1 && ivestis <= 2) || (meniu == 1 && ivestis >= 1 && ivestis <= 3) || (meniu == 2 && ivestis >= 1 && ivestis <= 6))
+                    if ((meniu == 0 && ivestis == 2) || (meniu == 1 && ivestis == 3))
+                        testi = false; // iseiti is programos
+                    else if ((meniu == 0 && ivestis == 1) || (meniu == 1 && ivestis == 1))
+                    {
+                        rezultatas = null;
+                        meniu = 2; // pasirinkti operacijos
+                    }
+                    else if (meniu == 2 && (ivestis >= 1 && ivestis <= 6))
+                    {
+                        veiksmas = ivestis;
+                        Console.Write("pasirinktas veiksmas ");
+                        switch (veiksmas)
+                        {
+                            case 1:
+                                Console.WriteLine("+");
+                                break;
+                            case 2:
+                                Console.WriteLine("-");
+                                break;
+                            case 3:
+                                Console.WriteLine("*");
+                                break;
+                            case 4:
+                                Console.WriteLine("/");
+                                break;
+                            case 5:
+                                Console.WriteLine("^");
+                                break;
+                            case 6:
+                                Console.WriteLine("√");
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        if (rezultatas != null)
+                        {
+                            //jeigu naudojam esama rezultata
+                            skaicius1 = rezultatas;
+                            if (veiksmas == 6 )
+                                skaicius2 = rezultatas; // saknes traukimui naudojame esame rezultata
+                            else meniu = 4; // ivesti antra skaiciu
+                        }
+                        else if(veiksmas == 6)
+                        {
+                            meniu = 5;
+                        }
+                        else
+                            meniu = 3;// ivesti pirma skaiciu
+                    }
+                    else
+                        meniu = ivestis;
+                else
+                    klaida = 1;// klaida nera tokio meniu
+            }
+
+            // 1 skaiciu ivedimas
+            else if (ivedimas.Length > 0 && meniu == 3  && skaicius1 == null && klaida == 0 && veiksmas != null )
+            {
+                skaicius1 = DoubleSkaiciausTikrinimas(ivedimas);
+                if (skaicius1 == null) 
+                    klaida = 2;
+                else 
+                    meniu = 4; 
+            }
+            // 2 skaiciaus ivedimas
+            else if (ivedimas.Length > 0 && (meniu == 4 || meniu == 5) && skaicius2 == null && klaida == 0 && veiksmas != null)
+            {
+                if (veiksmas == 6 && skaicius1 != null ) // saknes traukimas
+                    skaicius2 = skaicius1; // saknes traukimo atveju su esamu rezultatu fiktyviai priskiriam skaicius2 tam, kad butu laikoma kad turime 2 ivestus skaicius ir ivyktu apskaiciavimas 
+                else if (veiksmas == 6 && skaicius1 == null)
+                    {
+                    skaicius2 = DoubleSkaiciausTikrinimas(ivedimas);
+                    skaicius1 = skaicius2; // saknes traukimui reikia tik vieno parametro todel fiktyviai priskiriam.
+                    }
+                else
+                    skaicius2 = DoubleSkaiciausTikrinimas(ivedimas);
+
+                
+                if (skaicius2 == null)  // ivestas ne skaicius
+                    klaida = 2;
+                else if (skaicius2 == 0 && veiksmas == 4) // dalyba is nullio
+                {
+                    klaida = 3;
+                    skaicius2 = null; // anuliojame antra skaiciu, kad galima butu ivesti pakartotinai
+                }
+            }
+
+
+            if (klaida == 0 && veiksmas != null && skaicius1 != null && skaicius2 != null)
+            {
+                switch (veiksmas) // jeigu yra 2 skaiciai tada apskaiciuojame rezultata
+                {
+                    case 1:
+                        rezultatas = Sudetis((double)skaicius1, (double)skaicius2);
+                        break;
+                    case 2:
+                        rezultatas = Atimtis((double)skaicius1, (double)skaicius2);
+                        break;
+                    case 3:
+                        rezultatas = Daugyba((double)skaicius1, (double)skaicius2);
+                        break;
+                    case 4:
+                        rezultatas = Dalyba((double)skaicius1, (double)skaicius2);
+                        break;
+                    case 5:
+                        rezultatas = Math.Pow((double)skaicius1, (double)skaicius2);
+                        break;
+                    case 6:
+                        rezultatas = Math.Sqrt((double)skaicius2);
+                        break;
+                    default:
+                        break;
+                }
+                if (klaida == 0 && rezultatas != null)
+                    Console.WriteLine("Rezultatas: {0}", rezultatas);
+                skaicius1 = null;
+                skaicius2 = null;
+                veiksmas = null;
+                meniu = 1; // grizti i meniu
+            }
         }
 
         public static double Rezultatas()
@@ -80,9 +214,75 @@
         {
             //todo
             rezultatas = null;
+            skaicius1 = null;
+            skaicius2 = null;
+            veiksmas = null;
+            klaida = 0;
+            meniu = 0;
+        }
+
+        public static void PagrindinisMeniu()
+        {
+ 
+            Console.Clear();
+            Reset();
+            while (testi)
+            {
+                switch (meniu)
+                {
+                    case 0:  // pradinis meniu
+                        Console.WriteLine("\n1. Nauja operacija. 2. Išeiti");
+                        break;
+                    case 1: // pagrindinis meniu
+                        Console.WriteLine("\n1. Nauja operacija. 2.Testi su rezultatu. 3.Išeiti");
+                        break;
+                    case 2:   // sub meniu operacijos
+                        Console.WriteLine("1.Sudetis 2.Atimtis 3.Daugyba 4.Dalyba 5. Kelimas laipsniu 6. Kvadratinė šaknis");
+                        break;
+                    case 3: // pirmas skaicius
+                        Console.WriteLine("Įveskite pirma skaičiu");
+                        break;
+                    case 4: // antras skaicius
+                        Console.WriteLine("Įveskite antra skaičiu");
+                        break;
+                    case 5: // antras skaicius
+                        Console.WriteLine("Įveskite skaičiu");
+                        break;
+                    default:
+                        
+                        break;
+                }
+
+                SuperSkaiciuotuvas(Console.ReadLine());
+
+                switch (klaida)
+                {
+                    case 1:
+                        Console.WriteLine("Nėra tokio meniu pasirinkimo.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Neteisingas skaičiaus formatas.");
+                        klaida = 0;
+                        break;
+                    case 3:
+                        Console.WriteLine("Dalyba iš nulio.");
+                        klaida=0;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
         }
 
 
+        public static double Sudetis(double skaicius1, double skaicius2) => skaicius1 + skaicius2;
+        public static double Atimtis(double skaicius1, double skaicius2) => skaicius1 - skaicius2;
+        public static double Daugyba(double skaicius1, double skaicius2) => skaicius1 * skaicius2;
+        public static double Dalyba(double skaicius1, double skaicius2) => skaicius1 / skaicius2;
+
+        private static double? DoubleSkaiciausTikrinimas(string? tekstas) => double.TryParse(tekstas, out double skaicius) ? skaicius : null;
+        private static int IntSkaiciausTikrinimas(string? tekstas) => int.TryParse(tekstas, out int skaicius) ? skaicius : 0;
 
     }
 
--- BasicMokymai/SuperSkaiciuotuvas_Tests/UnitTest1.cs
+++ BasicMokymai/SuperSkaiciuotuvas_Tests/UnitTest1.cs
@@ -3,14 +3,7 @@ namespace SuperSkaiciuotuvas_Tests
     [TestClass]
     public class UnitTest1
     {
-        /*
-        [TestMethod]
-        public void TestMethod1()
-        {
-            var fake_moves = new string[] { "1", "1", "15", "45", "2", "2", "10", "3" };
-            SuperSkaiciuotuvas.Program.SuperSkaiciuotuvas();
-        }
-        */
+
         [TestMethod()]
         public void SuperSkaiciuotuvasTest1()
         {
@@ -57,6 +50,157 @@ namespace SuperSkaiciuotuvas_Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+       
+        [TestMethod()] // laukio pakartotinio antro skaiciaus ivedimo
+        public void SuperSkaiciuotuvasDalybaIsNulio()
+        {
+            var fake_moves = new string[] { "1", "4", "100", "0" };
+            var expected = 0d;
+            SuperSkaiciuotuvas.Program.Reset();
+            foreach (var move in fake_moves)
+            {
+                SuperSkaiciuotuvas.Program.SuperSkaiciuotuvas(move);
+            }
+            var actual = SuperSkaiciuotuvas.Program.Rezultatas();
+     
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()] // po klaidos pakartotinai ivedamas antras skaicius ir tada atliekama dalyba
+        public void SuperSkaiciuotuvasDalybaIsNulioSuIvedimoPakartojimu()
+        {
+            var fake_moves = new string[] { "1", "4", "100", "0", "2" };
+            var expected = 50d;
+            SuperSkaiciuotuvas.Program.Reset();
+            foreach (var move in fake_moves)
+            {
+                SuperSkaiciuotuvas.Program.SuperSkaiciuotuvas(move);
+            }
+            var actual = SuperSkaiciuotuvas.Program.Rezultatas();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()] // po formatavimo klaidos pakartotinai ivedamas antras skaicius ir tada atliekama daugyba
+        public void SuperSkaiciuotuvasDaugybaBlogaiIvestasSkaiciusSuIvedimoPakartojimu()
+        {
+            var fake_moves = new string[] { "1", "3", "dff", "40", "2", "3" };
+            var expected = 80d;
+            SuperSkaiciuotuvas.Program.Reset();
+            foreach (var move in fake_moves)
+            {
+                SuperSkaiciuotuvas.Program.SuperSkaiciuotuvas(move);
+            }
+            var actual = SuperSkaiciuotuvas.Program.Rezultatas();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()] // laipsnio kelimu ir pratesimas atimtis su esamu rezultatu
+        public void SuperSkaiciuotuvasKelimasLaipsiuSuAtimtiesTesimu()
+        {
+            var fake_moves = new string[] { "1", "5", "2", "3", "2", "2","3","3" };
+            var expected = 5d;
+            SuperSkaiciuotuvas.Program.Reset();
+            foreach (var move in fake_moves)
+
+            {
+                SuperSkaiciuotuvas.Program.SuperSkaiciuotuvas(move);
+            }
+            var actual = SuperSkaiciuotuvas.Program.Rezultatas();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()] // kvadratines saknies traukimas
+        public void SuperSkaiciuotuvasSakniesTraukimas()
+        {
+            var fake_moves = new string[] { "1", "6", "5", "3" };
+            var expected = 2.23606797749979d;
+            SuperSkaiciuotuvas.Program.Reset();
+            foreach (var move in fake_moves)
+
+            {
+                SuperSkaiciuotuvas.Program.SuperSkaiciuotuvas(move);
+            }
+            var actual = SuperSkaiciuotuvas.Program.Rezultatas();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()] // kvadratines saknies traukimas su atimties tesimu 
+        public void SuperSkaiciuotuvasSakniesTraukimasSuAtimtiesTesimu()
+        {
+            var fake_moves = new string[] { "1", "6", "25", "2", "2", "2", "3" };
+            var expected = 3d;
+            SuperSkaiciuotuvas.Program.Reset();
+            foreach (var move in fake_moves)
+
+            {
+                SuperSkaiciuotuvas.Program.SuperSkaiciuotuvas(move);
+            }
+            var actual = SuperSkaiciuotuvas.Program.Rezultatas();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void MetodasSudetis1()
+        {
+            var skacius1 = 1d;
+            var skacius2 = 2d;
+            var expected = 3d;
+
+            var actual = SuperSkaiciuotuvas.Program.Sudetis(skacius1, skacius2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MetodasAtimstis1()
+        {
+            var skacius1 = 1d;
+            var skacius2 = 2d;
+            var expected = -1d;
+
+            var actual = SuperSkaiciuotuvas.Program.Atimtis(skacius1, skacius2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MetodasDaugyba1()
+        {
+            var skacius1 = 4d;
+            var skacius2 = 2d;
+            var expected = 8d;
+
+            var actual = SuperSkaiciuotuvas.Program.Daugyba(skacius1, skacius2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MetodasDalyba1()
+        {
+            var skacius1 = 25d;
+            var skacius2 = 5d;
+            var expected = 5d;
+
+            var actual = SuperSkaiciuotuvas.Program.Dalyba(skacius1, skacius2);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MetodasDalybaIsNulio()
+        {
+            var skacius1 = 25d;
+            var skacius2 = 0d;
+            var expected = double.PositiveInfinity;
+
+            var actual = SuperSkaiciuotuvas.Program.Dalyba(skacius1, skacius2);
+            Assert.AreEqual(expected, actual);
+        }
+
 
     }
 }
