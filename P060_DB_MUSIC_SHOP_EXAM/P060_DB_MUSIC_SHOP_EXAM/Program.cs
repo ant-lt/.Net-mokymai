bd21c392afc4a9157dff65c344082eb815810c6d
Vitas Gruzdas <v.gruzdas74@gmail.com>
2022-09-28 19:08:10 +0300

    Padaryta direktorijų ir suinstaliuoti EF

---
+++ P060_DB_MUSIC_SHOP_EXAM/P060_DB_MUSIC_SHOP_EXAM/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P060_DB_MUSIC_SHOP_EXAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
